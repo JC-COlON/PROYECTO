2eca2524b5f8fbb0235e918e50a54f68653073db
JR-COLON <colonleonardocolon@gmail.com>
2021-04-27 23:20:12 -0600

    Subiendos Los archivos

---
+++ Pages/NewFolder/IndexHome.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PROYECTO_ESCUELA_DE_INGLES.Pages.NewFolder
+{
+    public class IndexHomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
